2115b515bee720adcf153600a8600dbf6360fcdf
Jesse Aldridge <jessealdridge@gmail.com>
2021-04-19 20:42:37 -0500

    update demo

--- demo.js
+++ demo.js
@@ -3,7 +3,8 @@ const fs_warehouser = require('./index')
 fs_warehouser.run_loop({
   name: 'test scraper',
   main: async (is_test, write_to_scraper_dir) => {
-    const page = fs_warehouser.launch_puppeteer()
+    const page = await fs_warehouser.launch_puppeteer()
+    page.goto('https://google.com')
     console.log("this is a fake scraper that doesn't do anything")
     write_to_scraper_dir('fake_scraper', [1,2,3])
   }
