3825b95418e630c6e70920d23947e874c4097f5b
Jesse Aldridge <jessealdridge@gmail.com>
2022-03-31 22:33:53 -0500

    finish python version

--- fs_warehouser/fs_warehouser.py
+++ fs_warehouser/fs_warehouser.py
@@ -1,85 +1,77 @@
-import os, csv, glob, logging, sys
+import os, csv, glob, logging, sys, traceback, time, json
+from textwrap import indent
 from datetime import datetime
 
 import sane_logger
 
 
 logger = sane_logger.sane_logger()
-logger.info('unbiasstock trades...')
-def run_loop(scraper, data_dir_path, is_test, sleep_time):
+
+def run_loop(scraper_func, data_dir_path, is_test, sleep_ms):
   data_dir_path = os.path.expanduser(data_dir_path)
-  date_str = str(datetime.utcnow()).replace(' ', '_').replace(':', '-').replace('.', '-')
-  # 2016-08-15_21-11-20-492714
 
-  try:
-    await run_once(scraper, is_test, date_str, data_dir_path)
-  except Exception as exc: {
-    logger.error('scraper error:', err);
-    console.log(err.stack);
-  }
+  while True:
+    date_str = str(datetime.utcnow()).replace(' ', '_').replace(':', '-').replace('.', '-')
+    # 2016-08-15_21-11-20-492714
 
-  sleep_time = sleep_time || 24 * 60 * 60 * 1000
-  timestamped_log(`${new Date()}, done scraping, sleeping for ${sleep_time / 1000 / 60 / 60} hours`)
-  const original_arguments = arguments
-  setTimeout(() => {
-    await run_loop(scraper, data_dir_path, is_test, sleep_time)
-  }, sleep_time || 1000 * 60 * 60 * 24)
+    try:
+      run_once(scraper_func, is_test, date_str, data_dir_path)
+    except Exception as exc:
+      logger.error(f"Scraper error: {exc}, {exc.args}")
+      traceback.print_exc()
 
-def run_once(scraper, is_test, date_str, data_dir_path) {
-  timestamped_log('running scraper')
+    sleep_ms = sleep_ms or (24 * 60 * 60 * 1000)
+    logger.info(f'done scraping, sleeping for {sleep_ms / 1000 / 60 / 60} hours')
+    time.sleep(sleep_ms / 1000 or 60 * 60 * 24)
 
-  def write_to_scraper_dir(filename, obj) {
-    const file_path = path.join(data_dir_path, date_str, filename)
-    const file_path_with_exten = write_file(file_path, obj)
-    timestamped_log('wrote results to:', file_path_with_exten)
-  }
+def run_once(scraper_func, is_test, date_str, data_dir_path):
+  logger.info('running scraper')
 
-  await scraper.main(is_test, write_to_scraper_dir)
-}
+  def write_to_scraper_dir(filename, obj):
+    file_path = os.path.join(data_dir_path, date_str, filename)
+    file_path_with_exten = write_file(file_path, obj)
+    logger.info(f'wrote results to: {file_path_with_exten}')
 
-def write_file(file_path, obj) {
-  const dir_path = path.dirname(file_path)
-  if(!fs.existsSync(dir_path))
-    // is sync
-    shell.mkdir('-p', dir_path)
+  scraper_func(is_test, write_to_scraper_dir)
 
-  let text = null, exten = null
-  if(Array.isArray(obj) && (obj.length == 0 || Array.isArray(obj[0]))) {
-    exten = '.csv'
-    text = Papa.unparse(obj)
-  }
-  else {
-    exten = '.json'
-    text = JSON.stringify(obj, null, 2)
-  }
+def write_file(file_path, obj):
+  file_path = os.path.expanduser(file_path)
+  dir_path = os.path.dirname(file_path)
+  if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
 
-  file_path += exten
-  fs.writeFileSync(file_path, text)
+  text, exten = None, None
+  if file_path.endswith('csv'):
+    with open(file_path, 'w') as f:
+      csv.writer(f).writerows(obj)
+  else:
+    text = json.dumps(obj, indent=2)
+    with open(file_path, 'w') as f:
+      f.write(text)
   return file_path
-}
 
-def get_last_timestamped_dir_path(data_dir_path) {
-  const glob_path = path.join(expandHomeDir(data_dir_path), '2*')
-  const date_paths = glob.sync(glob_path)
+def get_last_timestamped_dir_path(data_dir_path):
+  glob_path = os.path.join(os.path.expanduser(data_dir_path), '2*')
+  date_paths = glob.glob(glob_path)
   date_paths.sort()
-  return date_paths[date_paths.length - 1] || null
-}
+  return date_paths[-1] if date_paths else None
 
-def launch_puppeteer() {
-  const browser = await puppeteer.launch({
-    headless: true,
-  });
-  const page = (await browser.pages())[0] || (await browser.newPage());
-  await page.setUserAgent(
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' +
-    'Chrome/61.0.3163.100 Safari/537.36'
-  );
-  return page;
-}
+def test():
+  print(get_last_timestamped_dir_path('~/fake_scraper_data'))
 
-if(typeof exports != 'undefined') {
-  exports.write_file = write_file
-  exports.get_last_timestamped_dir_path = get_last_timestamped_dir_path
-  exports.run_loop = run_loop
-  exports.launch_puppeteer = launch_puppeteer
-}
+  write_file('~/fake_scraper_data/test.json', {'x': 1})
+
+  def test_scraper_func(is_test, write_to_scraper_dir):
+    write_to_scraper_dir('foo.csv', [['y'], [2]])
+
+  run_once(
+    scraper_func=test_scraper_func,
+    is_test=True,
+    date_str='3000_01_01',
+    data_dir_path='~/fake_scraper_data',
+  )
+
+  run_loop(test_scraper_func, '~/fake_scraper_data', is_test=True, sleep_ms=1000)
+
+if __name__ == '__main__':
+  test()
