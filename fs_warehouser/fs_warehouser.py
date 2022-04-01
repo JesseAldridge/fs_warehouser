bfea1c7baf65867119ace1d479acf6f89ccce8aa
Jesse Aldridge <jessealdridge@gmail.com>
2022-04-01 15:33:58 -0500

    create logger and requester internally

--- fs_warehouser/fs_warehouser.py
+++ fs_warehouser/fs_warehouser.py
@@ -1,9 +1,12 @@
-from cmath import log
 import os, csv, glob, logging, sys, traceback, time, json
 from textwrap import indent
 from datetime import datetime
 
-def run_loop(scraper_func, data_dir_path, is_test, sleep_ms, logger):
+import sane_logger, robust_request
+
+def run_loop(scraper_func, data_dir_path, is_test, sleep_ms):
+  logger = sane_logger.sane_logger()
+  requester = robust_request.Requester(logger=logger)
   data_dir_path = os.path.expanduser(data_dir_path)
 
   while True:
@@ -11,7 +14,7 @@ def run_loop(scraper_func, data_dir_path, is_test, sleep_ms, logger):
     # 2016-08-15_21-11-20-492714
 
     try:
-      run_once(scraper_func, is_test, date_str, data_dir_path, logger)
+      run_once(scraper_func, is_test, date_str, data_dir_path, logger, requester)
     except Exception as exc:
       logger.error(f"Scraper error: {exc}, {exc.args}")
       traceback.print_exc()
@@ -20,7 +23,7 @@ def run_loop(scraper_func, data_dir_path, is_test, sleep_ms, logger):
     logger.info(f'done scraping, sleeping for {sleep_ms / 1000 / 60 / 6:0.4f} hours')
     time.sleep(sleep_ms / 1000 or 60 * 60 * 24)
 
-def run_once(scraper_func, is_test, date_str, data_dir_path, logger):
+def run_once(scraper_func, is_test, date_str, data_dir_path, logger, requester):
   logger.info('running scraper')
 
   def write_to_scraper_dir(filename, obj):
@@ -28,7 +31,7 @@ def run_once(scraper_func, is_test, date_str, data_dir_path, logger):
     file_path_with_exten = write_file(file_path, obj)
     logger.info(f'wrote results to: {file_path_with_exten}')
 
-  scraper_func(is_test, write_to_scraper_dir)
+  scraper_func(is_test, write_to_scraper_dir, logger, requester)
 
 def write_file(file_path, obj):
   file_path = os.path.expanduser(file_path)
@@ -56,27 +59,15 @@ def get_last_timestamped_dir_path(data_dir_path):
   return date_paths[-1] if date_paths else None
 
 def test():
-  import requests, sane_logger
-
-  logger = sane_logger.sane_logger()
-
   print(get_last_timestamped_dir_path('~/fake_scraper_data'))
 
   write_file('~/fake_scraper_data/test.json', {'x': 1})
 
-  def test_scraper_func(is_test, write_to_scraper_dir):
-    resp = requests.get('https://google.com')
+  def test_scraper_func(is_test, write_to_scraper_dir, logger, requester):
+    resp = requester.get('https://google.com')
     write_to_scraper_dir('google.html', resp.content)
 
-  run_once(
-    scraper_func=test_scraper_func,
-    is_test=True,
-    date_str='3000_01_01',
-    data_dir_path='~/fake_scraper_data',
-    logger=logger,
-  )
-
-  run_loop(test_scraper_func, '~/fake_scraper_data', is_test=True, sleep_ms=1000, logger=logger)
+  run_loop(test_scraper_func, '~/fake_scraper_data', is_test=True, sleep_ms=1000)
 
 if __name__ == '__main__':
   test()
